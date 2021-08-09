c66e861e6d76ef3a345884cd011540a2e4bf518d
LemonTimee <mustafasafasarioglu@gmail.com>
2021-08-09 17:15:58 +0300

    finish project

---
+++ app/src/main/java/com/engine/fragmentsnav/FirstFragment.kt
@@ -0,0 +1,32 @@
+package com.engine.fragmentsnav
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonFragment1.setOnClickListener {
+            val username = editTextFragment1.text.toString()
+
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(username)
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/engine/fragmentsnav/SecondFragment.kt
@@ -0,0 +1,35 @@
+package com.engine.fragmentsnav
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_second.*
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            val username = SecondFragmentArgs.fromBundle(it).username
+            textViewFragment2Welcome.text = "Welcome " + username
+        }
+
+        buttonFragment2.setOnClickListener {
+            val action = SecondFragmentDirections.actionSecondFragmentToFirstFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+}
